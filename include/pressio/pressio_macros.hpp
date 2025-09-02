2707cd69ff0b418b2a8b98425a567b055bebb304
Caleb Schilly <cwschilly@gmail.com>
2025-09-02 12:39:43 -0400

    #781: bump version number; correct line numbers

--- include/pressio/pressio_macros.hpp
+++ include/pressio/pressio_macros.hpp
@@ -52,7 +52,7 @@
 #include "pressio/ops_macros.hpp"
 
 #define PRESSIO_MAJOR_VERSION 0
-#define PRESSIO_MINOR_VERSION 16
+#define PRESSIO_MINOR_VERSION 17
 #define PRESSIO_PATCH_VERSION 0
 
 #ifdef PRESSIO_ENABLE_LOGGING
